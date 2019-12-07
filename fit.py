259e003708a5a8d2c75ecb05b21aa4e861cb719f
Tomasz Cygal <tomaszcygal@yahoo.com>
2019-12-07 01:00:44 +0000

    Started to connect flask with mongo DB

--- fit.py
+++ fit.py
@@ -1,9 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'fitness_pot'
+app.config["MONGO_URI"] = 'mongodb+srv://tomciosegal:cucharec7164@myfirstcluster-nbawd.mongodb.net/fitness_pot?retryWrites=true&w=majority'
 
-
+mongo = PyMongo(app)
 @app.route('/')
 def hello():
     return 'Hello World ...again'
