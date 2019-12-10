09a06db20139024318677a89f5acff0301dfc141
Tomasz Cygal <tomaszcygal@yahoo.com>
2019-12-10 12:51:15 +0000

    my recipies email and get my recpies done

--- fit.py
+++ fit.py
@@ -48,13 +48,13 @@ def home_page1():
      pprint.pprint(mycol.find_one({"category": "dinner"}))
      return mycol.find_one()
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/myrecipies')
+def myrecipies():
+    return render_template('myrecipies.html')
 
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+    app.run(host=os.environ.get('IP','0.0.0.0'),
+            port=int(os.environ.get('PORT','8080')),
             debug=True)
