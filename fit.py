b9b09e7cec8fb28c04e607b6aa74c95192691f5f
Tomasz Cygal <tomaszcygal@yahoo.com>
2019-12-13 19:14:48 +0000

    buttons sign up login wired and sending data to mongo

--- fit.py
+++ fit.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 
@@ -14,7 +14,7 @@ import pprint
 myclient = pymongo.MongoClient("mongodb+srv://tomciosegal:cucharec7164@myfirstcluster-nbawd.mongodb.net")
 mydb = myclient["fitness_pot"]
 mycol = mydb["dish"]
-
+userscol = mydb["users"]
 x = mydb.list_collection_names()
 @app.route('/')
 # def hello():
@@ -48,9 +48,47 @@ def home_page1():
      pprint.pprint(mycol.find_one({"category": "dinner"}))
      return mycol.find_one()
 
-@app.route('/myrecipies')
-def myrecipies():
-    return render_template('myrecipies.html')
+@app.route('/addrecipie')
+def addrecipie():
+    return render_template('addrecipie.html')
+
+@app.route('/create_user', methods = ['POST'])
+def createuser():
+    newuser = {
+        "username": request.form.get('username'),
+        "password": request.form.get('password'),
+        "email": request.form.get('email')
+    }
+    userscol.insert_one(newuser)
+    return 'SUCCESS'
+    
+@app.route('/login', methods = ['POST'])
+def login():
+    username =  request.form.get('username')
+    password = request.form.get('password')
+    userfromdb = userscol.find_one({ "username": username })
+    if (userfromdb != None and userfromdb['password'] == password):
+        return 'SUCCESS'
+    else:
+        return 'FAILED'
+   
+
+@app.route('/createrecepie', methods = ['POST'])
+def createrecepie():
+    print (request.form.get('user_name'))
+    newrecipe = {"title" :"test recepie", 
+              "category" : "vegan",
+              "image" : "test_image.jpeg",
+              "cooking_time" : "40",
+              "serves" : "2",
+              "calories" : "100",
+              "author" : "John",
+              "ingredients" : "a,b,c",
+              "instructions" : "dsfdgfdsf",
+              "nutrition" : "fbfgdsdf"
+        }
+    mycol.insert_one(newrecipe)
+    return 'TEST'
 
 
 
